62a38a5468f4dc28a6973727c51ce02b853a1eff
Marko <kostres90@hotmail.com>
2024-12-29 21:33:51 +0100

    Store Yelp API key in environment variable for improved security

--- food/src/api/yelp.js
+++ food/src/api/yelp.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
-    Authorization:
-      "Bearer QwAp_TNzIMNSznDhdHO2SYOP6THoeqeB5bOFSUN41-L-h2Ng8UkTdtjgEXmyMsUWnRk8A3e-cl92frCo693Y63iywEetJgYUntHTtSTXF4ZCQ4k98N-V-rWRicxVZ3Yx",
+    Authorization: `Bearer ${process.env.YELP_KEY}`,
   },
 });
