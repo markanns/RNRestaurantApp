acc618e16774a6d01784956353066872a7b687e7
Marko <kostres90@hotmail.com>
2024-12-29 21:14:39 +0100

    Enhance ResultsList and ResultsShowScreen components to handle empty results and display images from Yelp API

--- food/src/screens/ResultsShowScreen.js
+++ food/src/screens/ResultsShowScreen.js
@@ -1,10 +1,33 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, Image, Text, StyleSheet, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
+import yelp from "../api/yelp";
+const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
+  const id = navigation.getParam("id");
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+  useEffect(() => {
+    getResult(id);
+  }, []);
 
-const ResultsShowScreen = () => {
+  if (!result) return null;
   return (
     <View>
-      <Text>ResultsShowScreen</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={(item) => {
+          return (
+            <Image
+              source={{ uri: item.item }}
+              style={{ width: 300, height: 200 }}
+            />
+          );
+        }}
+      />
     </View>
   );
 };
