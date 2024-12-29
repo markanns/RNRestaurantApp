22877e2c3d043cdf4e092b6aea1d5913e68bf54a
Marko <kostres90@hotmail.com>
2024-12-29 21:40:33 +0100

    Rename Yelp API key environment variable for consistency with React Native configuration

--- food/src/api/yelp.js
+++ food/src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.yelp.com/v3/businesses",
   headers: {
-    Authorization: `Bearer ${process.env.YELP_KEY}`,
+    Authorization: `Bearer ${process.env.REACT_NATIVE_YELP_KEY}`,
   },
 });
